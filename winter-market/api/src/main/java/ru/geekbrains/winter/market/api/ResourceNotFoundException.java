55e784f294d3d10e95f54e30fb08f20a31628e9c
sd351 <sd351@yandex.ru>
2022-03-28 14:59:04 +0300

    lesson 8

--- winter-market/api/src/main/java/ru/geekbrains/winter/market/api/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.winter.market.api;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- winter-market/core-service/src/main/java/ru/geekbrains/winter/market/core/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.winter.market.core.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.geekbrains.winter.market.core.entities.Order;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
